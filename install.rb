6239622a8a77eadda163f25616cad290521af7b0
Ben Woosley <ben.woosley@gmail.com>
2008-07-24 01:54:35 -0700

    Make install.rb runable as a standalone script, and robust to javascript file additions

--- install.rb
+++ install.rb
@@ -1,9 +1,14 @@
+#!/usr/bin/env ruby
+require File.join(File.dirname(__FILE__), '../../../config/boot')
+require 'fileutils'
+require 'pathname'
+
 # Install hook code here
 puts "Copying files..."
 dir = "javascripts"
-["jquery-ui.js", "jquery.js", "jrails.js"].each do |js_file|
-	dest_file = File.join(RAILS_ROOT, "public", dir, js_file)
-	src_file = File.join(File.dirname(__FILE__) , dir, js_file)
-	FileUtils.cp_r(src_file, dest_file)
-end
+src_path = Pathname(File.join(File.dirname(__FILE__), dir))
+paths = src_path.children.reject{|p| p.directory? }
+
+dest_path = File.join(RAILS_ROOT, "public", dir)
+FileUtils.cp(paths, dest_path)
 puts "Files copied - Installation complete!"
